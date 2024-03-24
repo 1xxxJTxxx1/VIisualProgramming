51fa1846d809fe45247bd7373590d347ce650933
1xxxJTxxx1 <timoxin_zenia80@mail.ru>
2024-03-24 16:15:34 +0300

    VP_Pract07

---
+++ VP_Pract07/ClassLibrary/ExistingItemCodeException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ExistingItemCodeException: ApplicationException
+    {
+        public Item? Item 
+        {
+            get;
+        }
+        public  string? Data 
+        {
+            get; 
+        }
+
+        public ExistingItemCodeException(Item item) 
+        {
+            Data=item.ToString();
+            Item=item;
+        }
+    }
+}
---
+++ VP_Pract07/ClassLibrary/Shop.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibrary
+{
+    public class Shop
+    {
+        private List<Item> _items = new();
+
+        public  void addItem(Item newItem)
+        {
+            
+
+           foreach (Item item in _items)
+            {
+                if (item.Id == newItem.Id)
+                    throw new ExistingItemCodeException(newItem);
+                
+            }
+           _items.Add(newItem);
+        }
+    }
+}
