2cc065e31100c6e9f3e85bb227339fb825c23e9f
1xxxJTxxx1 <timoxin_zenia80@mail.ru>
2024-03-30 22:41:38 +0300

    VP_Pract08

---
+++ VP_Ptact08/ClassLibrary/Item.cs
@@ -0,0 +1,84 @@
+﻿
+namespace ClassLibrary
+{
+    public class Item
+    {
+        private int id;
+
+        private string name;
+
+        private string color;
+
+        private double price;
+
+        private string provider;
+
+        private DateOnly productionDate;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            private set { }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set { }
+        }
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            private set { }
+        }
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            private set { }
+        }
+        public string Provider { 
+            get    
+            {
+                return provider;
+              }
+            private set { } 
+        }
+
+        public DateOnly ProductionDate
+        {
+            get
+            {
+                return productionDate;
+            }
+            private set { }
+        }
+
+    
+
+        public Item(int id, string name, string color, double price, string provider, DateOnly productionDate)
+        {
+            this.id = id;
+            this.name = name;
+            this.color = color;
+            this.price = price;
+            this.provider = provider;
+            this.productionDate = productionDate;
+        }
+        public override string ToString()
+        {
+            return $"\tАртикул {id}. Наименование {name}. Цвет {color}. Стоимость {price}. Поставщик {provider}. Дата производства {productionDate}\n";
+        }
+    }
+}
